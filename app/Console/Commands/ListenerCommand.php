4734feb50fd0ec863df7c5f82c6bb9205aa0038c
Ярослав Гошовський <goszowski@gmail.com>
2023-06-05 19:38:42 +0300

    parsing

--- app/Console/Commands/ListenerCommand.php
+++ app/Console/Commands/ListenerCommand.php
@@ -41,7 +41,20 @@ class ListenerCommand extends Command
             $server->on('message', function($message, $address, $server) {
                 // $server->send('hello ' . $address . '! echo: ' . $message, $address);
 
-                $this->info('client ' . $address . ': ' . $message);
+                // $this->info('client ' . $address . ': ' . $message);
+
+                $pattern = "/^\<([0-9]{1,3})\>([A-Za-z]{3} [0-9]{1,2} \d{2}:\d{2}:\d{2}) ([\S]+) ([\S]+) ([\S\s]+)/";
+                $result = preg_split($pattern, $message, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+
+                $data = [
+                    'priority' => $result[0],
+                    'date' => $result[1],
+                    'server' => $result[2],
+                    'app' => $result[3],
+                    'message' => $result[4],
+                ];
+
+                $this->info("Priority: {$data['priority']}\nServer: {$data['server']}\nApp: {$data['app']}\nMessage: {$data['message']}");
             });
         });
 
